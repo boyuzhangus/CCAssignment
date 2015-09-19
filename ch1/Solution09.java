97c3b3fb4de5e6a989399dd61e89dfe86a2c16b0
boyuzhangus <boyuzhangus@gmail.com>
2015-09-19 15:09:20 -0400

    Update Solution09.java

--- ch1/Solution09.java
+++ ch1/Solution09.java
@@ -1,8 +1,14 @@
 public class Solution09{
+	public static boolean isSubstring(String str1, String str2) {
+		return str2.contains(str1);
+	}
 	public static boolean isRotation(String s1, String s2){
 		if(s1.length() != s2.length()) //Judge the given length
 			return false;
 		String str = s1+s1;  //concate two same s1 to construct a useful string
-		return isSubstring(s2, str);
+		return Solution09. isSubstring(s2, str);
+	}
+	public static void main(String []args) {
+		System.out.println(Solution09.isRotation("waterbottle","erbottlewat")); //should be true
 	}
 }
