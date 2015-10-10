7879e89ab53e44f96235d233512a185770ccd11e
boyuzhangus <boyuzhangus@gmail.com>
2015-10-10 17:25:10 -0400

    Create SortStack.java

---
+++ ch3/SortStack.java
@@ -0,0 +1,12 @@
+//standard solution for record
+Stack<Integer> sort(Stack<Integer> s) {
+  Stack<Integer> r = new Stack<Integer>();
+  while(!s.isEmpty) {
+    int tmp = s.pop();
+    while(!r.isEmpty() && r.peek() > tmp) {
+      s.push(r.pop());
+    }
+    r.push(tmp);
+  }
+  return r;
+}
