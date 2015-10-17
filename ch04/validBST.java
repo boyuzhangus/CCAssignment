17ce2e96beb06c803dc6ca9da3c20766365c57e0
boyuzhangus <boyuzhangus@gmail.com>
2015-10-16 23:57:28 -0400

    Update validBST.java

--- ch04/validBST.java
+++ ch04/validBST.java
@@ -33,3 +33,21 @@ boolean checkBST(TreeNode n) {
   
   return true;
 }
+// Optimal Solution
+boolean checkBST(TreeNode n) {
+    return checkBST(n, null, null);
+}
+
+boolean checkBST(TreeNode n, Integer min, Integer max) {
+    if (n == null) {
+      return true;
+    }
+    if ((min != null && n.data <= min) || (max != null && n.data > max)) {
+      return false;
+    }
+    
+    if (!checkBST(n.left, min, n.data) || !checkBST(n.right, n.data, max)) {
+        return false;
+    }
+    return true;
+}
