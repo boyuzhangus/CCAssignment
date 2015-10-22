67e275f0dad1a6eb7c8b4592bce40f69ecf8092b
boyuzhangus <boyuzhangus@gmail.com>
2015-10-21 20:24:44 -0400

    Create BSTsequence.java

---
+++ ch04/BSTsequence.java
@@ -0,0 +1,47 @@
+ArrayList<LinkedList<Integer>> allSequences(TreeNode node) {
+	ArrayList<LinkedList<Integer>> result = new ArrayList<LinkedList<Integer>>();
+
+	if(node == null){
+		result.add(new LinkedList<Integer>());
+		return result;
+	}
+
+	LinkedList<Integer> prefix = new LinkedList<Integer>();
+	prefix.add(node.data);
+
+	//Recurse on left and right subtrees
+	ArrayList<LinkedList<Integer>> leftSeq = allSequences(node.left);
+	ArrayList<LinkedList<Integer>> rightSeq = allSequences(node.right);
+
+	for(LinkedList<Integer> left: leftSeq){
+		for(LinkedList<Integer> right: rightSeq){
+			ArrayList<LinkedList<Integer>> weaved = new ArrayList<LinkedList<Integer>>();
+			weaveLists(left, right, weaved, prefix);
+			result.addAll(weaved);
+		}
+	}
+	return result;
+}
+
+void weaveLists(LinkedList<Integer> first, LinkedList<Integer> second, ArrayList<LinkedList<Integer>> results,LinkedList<Integer> prefix){
+	if (first.size() == 0|| second.size() == 0){
+		LinkedList<Integer> result = (LinkedList<Integer>) prefix.clone();
+		result.addAll(first);
+		result.addAll(second);
+		results.add(result);
+		return;
+	}
+
+	int headFirst = first.removeFirst();
+	prefix.addLast(headFirst);
+	weaveLists(first,second,result,prefix);
+	prefix.removeLast();
+	first.addFirst(headFirst);
+
+	int headSecond = second.removeFirst();
+	prefix.addLast(headSecond);
+	weaveLists(first, second, results, prefix);
+	prefix.removeLast();
+	second.addFirst(headSecond);
+}
+ 
